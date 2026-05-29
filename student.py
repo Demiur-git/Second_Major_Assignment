7a66f0c906752116ca7857e524642508cedad505
Demiur-git <1966276457@qq.com>
2026-05-29 15:29:02 +0800

    定义Student类并且对Student类中的属性进行初始化

---
+++ student.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self,number,name,gender,class_number,student_id,department):
+        self.number = number
+        self.name = name
+        self.gender = gender
+        self.class_number = class_number
+        self.student_id = student_id
+        self.department = department
+    
