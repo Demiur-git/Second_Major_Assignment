a48a772c2238f1c4ba56b0b2d57fbbb458f96c92
Demiur-git <1966276457@qq.com>
2026-06-01 22:44:11 +0800

    完成run函数，并定义其他的空函数作为占位

--- exam_system.py
+++ exam_system.py
@@ -46,4 +46,47 @@ class ExamSys:
 
             self.students.append(student.Student(number, name, gender, class_number, student_id,department))
         #输出学生总数，便于检测有没有把所有学生信息成功载入
-        print(f"成功加载{len(self.students)}名学生的信息")
+        print(f"[系统]已成功加载{len(self.students)}名学生的信息")
+
+    def run(self):
+        #循环输出功能菜单并且进行功能调用，满足用户对于使用多项功能的需求
+        while True:
+            print("===== 学生信息与考场管理系统 =====")
+            print("1. 查询学生信息")
+            print("2. 随机点名")
+            print("3. 生成考场安排表")
+            print("4. 生成准考证文件")
+            print("+--------------------------------------------------------------------------")
+            print("0. 退出系统")
+            function_number=input("请输入功能编号：")
+            #如果输入不是正确的编号则要求重新输入
+            while function_number!='0' or function_number!='1' or function_number!='2' or function_number!='3' or function_number!='4':
+                function_number=input("功能编号不存在，请正确输入功能编号（0~4）：")
+            #根据输入的编号调用函数
+            if function_number=='0':
+                print("感谢使用，系统已退出。再见")
+                break
+            elif function_number=='1':
+                self.find_student()
+            elif function_number=='2':
+                self.random_roll_call(int(input("请输入你想要随即抽取多少名学生：")))
+            elif function_number=='3':
+                self.generate_exam_arrangement()
+            else:
+                self.generate_admission_tickets()
+
+    def find_student(self):
+        #占位
+        pass
+
+    def random_roll_call(self, number):
+        #占位
+        pass
+
+    def generate_exam_arrangement(self):
+        #占位
+        pass
+
+    def generate_admission_tickets(self):
+        #占位
+        pass
